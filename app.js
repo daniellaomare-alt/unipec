6f9789e663a473a28bc19b11f8487f717cb12fae
daniellaomare-alt <daniellaomare@gmail.com>
2026-05-07 19:32:56 +0200

    Add functions to manage parents, lessons, and schedule

---
+++ app.js
@@ -0,0 +1,11 @@
+function showParents() {
+  document.getElementById("output").innerHTML = "Parent Management Section";
+}
+
+function showLessons() {
+  document.getElementById("output").innerHTML = "Lesson Manager Section";
+}
+
+function showSchedule() {
+  document.getElementById("output").innerHTML = "Reminder Scheduling Section";
+}
