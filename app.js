142ac9bc368e63f4067df219818ef86c5f30f4db
daniellaomare-alt <daniellaomare@gmail.com>
2026-05-07 19:36:51 +0200

    Implement parent data addition and display
    Added functionality to add and display parent information.

--- app.js
+++ app.js
@@ -9,3 +9,34 @@ function showLessons() {
 function showSchedule() {
   document.getElementById("output").innerHTML = "Reminder Scheduling Section";
 }
+let parents = [];
+
+function addParent() {
+  const parentName = document.getElementById("parentName").value;
+  const parentPhone = document.getElementById("parentPhone").value;
+  const childName = document.getElementById("childName").value;
+
+  const parentData = {
+    parentName,
+    parentPhone,
+    childName
+  };
+
+  parents.push(parentData);
+
+  displayParents();
+}
+
+function displayParents() {
+  const parentList = document.getElementById("parentList");
+
+  parentList.innerHTML = "";
+
+  parents.forEach((parent) => {
+    parentList.innerHTML += `
+      <li>
+        ${parent.parentName} - ${parent.parentPhone} - Child: ${parent.childName}
+      </li>
+    `;
+  });
+          }
